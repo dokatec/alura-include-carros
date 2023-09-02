56df63443424c6dbcc9e6b330f5c41ed94489b9c
Jhonatan Pacheco <47837212+dokatec@users.noreply.github.com>
2023-09-02 00:05:44 -0300

    update files javascript e separação das responsabilidades

---
+++ imagens.js
@@ -0,0 +1,10 @@
+//variaveis das imagens
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+
+function preload() {
+  imagemDaEstrada = loadImage("imagens/estrada.png");
+  imagemDoAtor = loadImage("imagens/ator-1.png");
+  imagemCarro = loadImage("imagens/carro-1.png");
+}
--- sketch.js
+++ sketch.js
@@ -1,22 +1,11 @@
-let imagemDaEstrada;
-let imagemDoAtor;
-let imagemCarro;
-
-//variaveis carro
-let xCarro = 420;
-
-function preload() {
-  imagemDaEstrada = loadImage("imagens/estrada.png");
-  imagemDoAtor = loadImage("imagens/ator-1.png");
-  imagemCarro = loadImage("imagens/carro-1.png");
-}
-
 function setup() {
   createCanvas(500, 400);
 }
 
 function draw() {
   background(imagemDaEstrada);
-  image(imagemDoAtor, 100, 366, 30, 30);
-  image(imagemCarro, xCarro, 40, 50, 40);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+  movimentaAtor();
 }
