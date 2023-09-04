aee38c156aaf07d842868f0df49fb7c5678a327e
Jhonatan Pacheco <47837212+dokatec@users.noreply.github.com>
2023-09-03 21:33:03 -0300

    update files

--- ator.js
+++ ator.js
@@ -1,5 +1,7 @@
 //variaveis do ator
+let xAtor = 100;
 let yAtor = 366;
+let colisao = false;
 
 function mostraAtor() {
   image(imagemDoAtor, 100, yAtor, 30, 30);
@@ -12,4 +14,25 @@ function movimentaAtor() {
   if (keyIsDown(DOWN_ARROW)) {
     yAtor += 3;
   }
+}
+
+function verificaColisão() {
+  for (let i = 0; i < imagemCarros.length; i = i + 1) {
+    colisao = collideRectCircle(
+      xCarros[i],
+      yCarros[i],
+      comprimentoCarro,
+      alturaCarro,
+      xAtor,
+      yAtor,
+      15
+    );
+    if (colisao) {
+      colidiu();
+    }
+  }
+}
+
+function colidiu() {
+  yAtor = 366;
 }
